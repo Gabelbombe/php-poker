19fa3b3e55a976d5571f95aca5460ddd4e6dbd06
ehime <dodomeki@gmail.com>
2014-06-12 18:08:00 -0700

    DB setup incomplete

--- config/holdem.sql
+++ config/holdem.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE holdem;
+USE holdem;
+
 CREATE TABLE `session_store` (
   `id`                  bigint(20) NOT NULL AUTO_INCREMENT,
   `utid`                varchar(127) DEFAULT NULL,
@@ -8,5 +11,6 @@ CREATE TABLE `session_store` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
 
 Create User TexasHoldEmUser@'%' Identified By 'TexasHoldEmPass';
-Grant Insert, Select, Update, Delete On holdem To TexasHoldEmUser@'%';
+Grant Insert, Select, Update, Delete On holdem.* To TexasHoldEmUser@'%';
 
+FLUSH PRIVILEGES;
